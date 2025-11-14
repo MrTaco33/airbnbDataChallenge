179633bb7bfb2eeb6d5aa1bf9169f1b52fd7bff3
nicolejfre <nicolejim@hotmail.com>
2025-11-14 12:02:14 -0600

    Update 01_exploracion_inicial.R

--- scripts/01_exploracion_inicial.R
+++ scripts/01_exploracion_inicial.R
@@ -4,28 +4,35 @@ library(skimr)
 library(dplyr)
 
  # Cargar datos 
+#listings
 airbnb_listings <- read_csv("data/raw/listings.csv") %>%
   clean_names()
-airbnb_listings_scraped <- read_csv("data/raw/listings_scrapped.csv") %>%
-  clean_names()
-airbnb_reviews <- read_csv("data/raw/reviews.csv") %>%
-  clean_names()
 
+#listings_scrapped
+airbnb_listings_scraped <- read_csv("data/raw/listings_scrapped.csv") %>%
+  clean_names() %>%
+ airbnb_listings_scraped <- read_csv("data/raw/listings_scrapped.csv") %>%
+  clean_names() %>%
+  mutate(
+    price_num = parse_number(price)) %>%
+  filter(!is.na(price_num),
+    price_num > 0,
+    price_num < 10000)
+
+summary(airbnb_listings_scraped$price_num)
+
+#histograma de dist. del precio por noche
+ggplot(airbnb_listings_scraped, aes(x = price_num)) +
+  geom_histogram(bins = 60, fill = "skyblue", color = "white") +  
+  coord_cartesian(xlim = c(0, 5000)) +
+  labs(title = "Distribución del precio por noche (Airbnb CDMX)",
+    x = "Precio por noche (MXN)",
+    y = "Frecuencia") +
+  theme_minimal()
+
+#visualización rápida
 glimpse(airbnb_listings_scraped)
 skimr::skim(airbnb_listings_scraped$price)
-
-
-# Histograma de precios (listings_scrapped)
-airbnb_listings_scraped %>%
-  mutate(price_num = readr::parse_number(price)) %>%
-  ggplot(aes(x = price_num)) +
-  geom_histogram(bins = 60) +
-  coord_cartesian(xlim = c(0, 5000)) +
-  labs(
-    title = "Distribución del precio por noche (Airbnb CDMX)",
-    x = "Precio por noche (MXN)",
-    y = "Frecuencia"
-  )
 
 # Top colonias por número de anuncios
 airbnb_listings_scraped %>%
