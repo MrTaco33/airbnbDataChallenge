1606b8b97145a9bdd2a81ed40acfb91f0b30aa2a
nicolejfre <nicolejim@hotmail.com>
2025-11-14 11:34:57 -0600

    Update 01_exploracion_inicial.R

--- scripts/01_exploracion_inicial.R
+++ scripts/01_exploracion_inicial.R
@@ -6,8 +6,30 @@ library(dplyr)
  # Cargar datos 
 airbnb_listings <- read_csv("data/raw/listings.csv") %>%
   clean_names()
+airbnb_listings_scraped <- read_csv("data/raw/listings_scrapped.csv") %>%
+  clean_names()
 airbnb_reviews <- read_csv("data/raw/reviews.csv") %>%
   clean_names()
 
+glimpse(airbnb_listings_scraped)
+skimr::skim(airbnb_listings_scraped$price)
+
+
+# Histograma de precios (listings_scrapped)
+airbnb_listings_scraped %>%
+  mutate(price_num = readr::parse_number(price)) %>%
+  ggplot(aes(x = price_num)) +
+  geom_histogram(bins = 60) +
+  coord_cartesian(xlim = c(0, 5000)) +
+  labs(
+    title = "Distribución del precio por noche (Airbnb CDMX)",
+    x = "Precio por noche (MXN)",
+    y = "Frecuencia"
+  )
+
+# Top colonias por número de anuncios
+airbnb_listings_scraped %>%
+  count(neighbourhood_cleansed, sort = TRUE) %>%
+  slice_head(n = 20)
 
 
