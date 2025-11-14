9148e9315dfc57ac706db112d772ab92f8cccd56
nicolejfre <nicolejim@hotmail.com>
2025-11-14 13:04:22 -0600

    Update 01_exploracion_inicial.R

--- scripts/01_exploracion_inicial.R
+++ scripts/01_exploracion_inicial.R
@@ -11,32 +11,47 @@ airbnb_listings <- read_csv("data/raw/listings.csv") %>%
 #listings_scrapped
 airbnb_listings_scraped <- read_csv("data/raw/listings_scrapped.csv") %>%
   clean_names() %>%
- airbnb_listings_scraped <- read_csv("data/raw/listings_scrapped.csv") %>%
-  clean_names() %>%
   mutate(
     price_num = parse_number(price)) %>%
   filter(!is.na(price_num),
     price_num > 0,
-    price_num < 10000)
+    price_num < quantile(price_num, 0.99, na.rm = TRUE)) #para quitar el 1% +caro
 
 summary(airbnb_listings_scraped$price_num)
-
-#histograma de dist. del precio por noche
-ggplot(airbnb_listings_scraped, aes(x = price_num)) +
-  geom_histogram(bins = 60, fill = "skyblue", color = "white") +  
-  coord_cartesian(xlim = c(0, 5000)) +
-  labs(title = "Distribución del precio por noche (Airbnb CDMX)",
-    x = "Precio por noche (MXN)",
-    y = "Frecuencia") +
-  theme_minimal()
-
-#visualización rápida
 glimpse(airbnb_listings_scraped)
 skimr::skim(airbnb_listings_scraped$price)
 
-# Top colonias por número de anuncios
-airbnb_listings_scraped %>%
-  count(neighbourhood_cleansed, sort = TRUE) %>%
-  slice_head(n = 20)
+#Precio mediano por colonia
+precio_colonia <- airbnb_listings_scraped %>%
+  group_by(neighbourhood_cleansed) %>%
+  summarise(n=n(), mediana_precio=median(price_num, na.rm = TRUE)) %>%
+  filter(n>=30) %>%
+  arrange(desc(mediana_precio))
+
+#gráfica
+ggplot(precio_colonia,
+       aes(x = reorder(neighbourhood_cleansed, mediana_precio),
+           y = mediana_precio)) +
+  geom_col(fill = "skyblue") +
+  coord_flip() +
+  labs(
+    title = "Precio mediano por colonia (n ≥ 30)",
+    x = "Colonia",
+    y = "Precio mediano por noche (MXN)"
+  ) +
+  theme_minimal()
+
+#Histograma dist. del precio x noche
+ggplot(airbnb_listings_scraped, aes(x = price_num)) +
+  geom_histogram(bins = 60, fill = "skyblue", color = "white") +  
+  coord_cartesian(xlim = c(0, 5000)) +
+  labs(
+    title = "Distribución del precio por noche (Airbnb CDMX)",
+    x = "Precio por noche (MXN)",
+    y = "Frecuencia"
+  ) +
+  theme_minimal()
 
 
+#Calidad percibida: score de airbnb y sentimientos LLM
+#Tenemos que mergear review_sentiment_by_listing con listings_scrapped (en airbnb_panel)
